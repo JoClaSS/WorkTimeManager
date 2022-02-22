198229dee03e3db8bd810cf53d7c49d856d4b476
Claércio <j.claercio8@gmail.com>
2022-02-22 16:11:38 -0300

    Person

--- backend/server/bateponto/src/main/java/com/project/bateponto/model/Person.java
+++ backend/server/bateponto/src/main/java/com/project/bateponto/model/Person.java
@@ -1,16 +1,20 @@
 package com.project.bateponto.model;
 
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +24,39 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name="profiles")
+@Table(name="person")
 public class Person {
-
+	
+	  @Id
+	  @SequenceGenerator(name="seq",sequenceName="person_sequence",allocationSize = 0)
+	  @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
+	  private Long id;
+	  
+	  @Column(name="fullName")
+	  private String fullName;
+	  
+	  
+	  @Column(name="cpf")
+      private String cpf;
+	  
+	  @Column(name="email")
+      private String email;
+      
+	  @Column(name="phone")
+      private String phone;
+	  
+	  @Column(name="address")
+      private String address;
+	  
+	  @Column(name="birthDate")
+      private LocalDate birthDate;
+      
+	  @Column(name="gender")
+      private String gender;
+	  
+	  @CreationTimestamp
+	  @Column(name="sample_time")
+	  @Temporal(TemporalType.TIMESTAMP)
+	  private Date creationDate;
+         
 }
