27313383f8f6a9e9a9dbe0f972eca0f538abb49b
Claércio <j.claercio8@gmail.com>
2022-02-22 11:55:32 -0300

    Person

---
+++ backend/server/bateponto/src/main/java/com/project/bateponto/model/Person.java
@@ -0,0 +1,26 @@
+package com.project.bateponto.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="profiles")
+public class Person {
+
+}
