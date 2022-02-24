4f57cd14b9fd9c8e62036e9ae9d111fb95bb558e
Claércio <j.claercio8@gmail.com>
2022-02-24 11:58:22 -0300

    consultas SQL de Persons

--- backend/server/bateponto/src/main/java/com/project/bateponto/controller/PersonController.java
+++ backend/server/bateponto/src/main/java/com/project/bateponto/controller/PersonController.java
@@ -1,9 +1,13 @@
 package com.project.bateponto.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,13 @@ public class PersonController {
 		return this.personService.saveUpdate(newPerson);
 	}
 	
-
+    @GetMapping("/fullname")  // http://localhost:8080/person/fullname?name= *insert*
+    public Optional<Person> findByFullName(@RequestParam String name){
+    	  return this.personService.findByFullName(name);
+    }
+    
+    @GetMapping("/cpf") // http://localhost:8080/person/cpf?cpf= *insert*
+    public Optional<Person> findBycPF(@RequestParam String cpf){
+    	  return this.personService.findByCpf(cpf);
+    }
 }
--- backend/server/bateponto/src/main/java/com/project/bateponto/repository/PersonRepository.java
+++ backend/server/bateponto/src/main/java/com/project/bateponto/repository/PersonRepository.java
@@ -1,12 +1,18 @@
 package com.project.bateponto.repository;
 
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project.bateponto.model.Person;
 
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
-public interface PersonRepository extends JpaRepository<Person,Long> {
-
+	@Query(value = "SELECT * FROM person p WHERE (:full_name is null or p.full_name= :full_name)", nativeQuery = true)
+	Optional<Person> findByFullName(@Param("full_name") String fullName);
+	
+	@Query(value = "SELECT * FROM person p WHERE (:cpf is null or p.cpf= :cpf)", nativeQuery = true)
+	Optional<Person> findByCpf(@Param("cpf") String cpf);
 }
