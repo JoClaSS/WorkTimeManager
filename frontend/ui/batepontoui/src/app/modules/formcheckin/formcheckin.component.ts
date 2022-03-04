6afac4ae8aa0621601194c75d3fd8c456c77a606
Claércio <j.claercio8@gmail.com>
2022-03-04 16:46:03 -0300

    init checkIn form

--- frontend/ui/batepontoui/src/app/modules/formcheckin/formcheckin.component.ts
+++ frontend/ui/batepontoui/src/app/modules/formcheckin/formcheckin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Person } from 'src/app/shared/model/person';
 
 @Component({
   selector: 'app-formcheckin',
@@ -7,6 +8,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./formcheckin.component.css']
 })
 export class FormcheckinComponent implements OnInit {
+  personArray: Person[] | undefined;
+  selected!:string;
   public inForm!: FormGroup;
 
   constructor(private fb: FormBuilder) { }
--- frontend/ui/batepontoui/src/app/modules/modules.module.ts
+++ frontend/ui/batepontoui/src/app/modules/modules.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormcheckinComponent } from './formcheckin/formcheckin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     CommonModule,
     FormsModule,
     MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatInputModule,
     ReactiveFormsModule
   ],
   exports: [
