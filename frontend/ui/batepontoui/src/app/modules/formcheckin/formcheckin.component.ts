453462c66f427b049cd79799af8e731be23be306
Claércio <j.claercio8@gmail.com>
2022-03-08 14:16:43 -0300

    person Service

--- frontend/ui/batepontoui/src/app/modules/formcheckin/formcheckin.component.ts
+++ frontend/ui/batepontoui/src/app/modules/formcheckin/formcheckin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Check } from 'src/app/shared/model/check';
 import { Person } from 'src/app/shared/model/person';
 
 @Component({
@@ -8,13 +9,28 @@ import { Person } from 'src/app/shared/model/person';
   styleUrls: ['./formcheckin.component.css']
 })
 export class FormcheckinComponent implements OnInit {
-  personArray: Person[] | undefined;
+  personArray: Person[] = [];
+  checks!: Check;
   selected!:string;
   public inForm!: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
+  formulario(){
+    this.inForm = this.fb.group({
+      fullName: ['Jéssica Motossera', [Validators.required]],
+      sexo: ['Feminino'],
+      cpf: ['12345678910'],
+      email: ['jess@email.com'],
+      telefone: ['123456789'],
+      birthDate: ['12/12/2012'],
+      today: ['08/03/2022', [Validators.required]],
+      checkin: ['12:40', [Validators.required]]
+    });
+  }
+
   ngOnInit(): void {
+    this.formulario();
   }
 
 }
--- frontend/ui/batepontoui/src/app/shared/service/person.service.ts
+++ frontend/ui/batepontoui/src/app/shared/service/person.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Person } from '../model/person';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
+  apiUrl = 'http://localhost:8080/person';
+  findByfullName = 'http://localhost:8080/person/fullname?name=';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getPersonByFullName(){
+    return this.httpClient.get<Person>(this.findByfullName);
+  }
+
+  public getAll(){
+    return this.httpClient.get<Person[]>(this.apiUrl);
+  }
 }
+
+
