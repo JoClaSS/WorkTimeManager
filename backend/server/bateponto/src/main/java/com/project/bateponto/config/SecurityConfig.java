a0e2cdbef403cd23a3434dc9bb3dedd3ddf9cf86
Claércio <j.claercio8@gmail.com>
2022-04-01 16:32:42 -0300

    init authentication

---
+++ backend/server/bateponto/src/main/java/com/project/bateponto/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.project.bateponto.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SecurityConfig {
+
+}
